2d9eb5aabe68516b83bd3f8646a9717dce3db25a
wshi8912 <s.wa8912shi@gmail.com>
2025-09-22 17:26:47 +0900

    wip

--- src/app/(app)/core/page.tsx
+++ src/app/(app)/core/page.tsx
@@ -39,12 +39,6 @@ export default async function CorePage() {
 
   return (
     <div className='container mx-auto max-w-5xl px-4 py-8'>
-      <div className='mb-6 flex items-center justify-between'>
-        <h1 className='text-3xl font-semibold text-foreground'>Today</h1>
-        <form action={signOutAction}>
-          <Button type='submit' variant='outline'>Log Out</Button>
-        </form>
-      </div>
       <CoreView tz={tz} timed={timed} untimed={untimed} />
     </div>
   );
